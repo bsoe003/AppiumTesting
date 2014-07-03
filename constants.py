53eedc189dc95f9500d52fd1228e92e2c3402fb9
Brian Soe <bsoe@ucsd.edu>
2014-07-03 15:27:33 -0400

    constants

---
+++ constants.py
@@ -0,0 +1,8 @@
+platform = 'Android'
+version = '4.4.2'
+apk = 'Netflix-3.4.0-build-1492.apk'
+package = 'com.netflix.mediaclient'
+activity = '.ui.login.LoginActivity'
+host = 'http://localhost:4723/wd/hub'
+username = "janeviasat@gmail.com"
+password = "LeapFrog11"
